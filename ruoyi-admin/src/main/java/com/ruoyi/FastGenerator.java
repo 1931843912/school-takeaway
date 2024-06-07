ba2b7c95dd82172417a721129eef47b5ee5c2f72
chenguanzhou <2050743162@qq.com>
2024-06-07 22:59:45 +0800

    fix: 修复完善代码生成器

--- ruoyi-admin/src/main/java/com/ruoyi/FastGenerator.java
+++ ruoyi-admin/src/main/java/com/ruoyi/FastGenerator.java
@@ -22,15 +22,15 @@ public class FastGenerator {
                     builder.author("ChenGuanZhou") // 设置作者
                             .enableSwagger() // 开启 swagger 模式
                             .fileOverride() // 覆盖已生成文件
-                            .outputDir("D:\\gitProject\\schooltakeaway\\ruoyi-admin\\src\\main\\java\\com\\ruoyi\\web"); // 指定输出目录
+                            .outputDir("D:\\gitProject\\schooltakeaway\\ruoyi-admin\\src\\main\\java\\com\\ruoyi\\generator"); // 指定输出目录
                 })
                 .packageConfig(builder -> {
-                    builder.parent("com.ruoyi.generator") // 设置父包名
-                            .moduleName("FATHER") // 设置父包模块名
-                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D:\\gitProject\\schooltakeaway\\ruoyi-admin\\src\\main\\java\\com\\ruoyi\\web\\mapper")); // 设置mapperXml生成路径
+                    builder.parent("dish") // 设置父包名
+                            .moduleName("") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.xml, "D:\\gitProject\\schooltakeaway\\ruoyi-admin\\src\\main\\resources\\Mapper")); // 设置mapperXml生成路径
                 })
                 .strategyConfig(builder -> {
-                    builder.addInclude("sys_employee") // 设置需要生成的表名
+                    builder.addInclude("sys_dish") // 设置需要生成的表名
                             .addTablePrefix("t_", "c_"); // 设置过滤表前缀
                 })
                 .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
