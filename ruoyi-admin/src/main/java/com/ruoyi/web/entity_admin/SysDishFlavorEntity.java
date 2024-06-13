a730c6f0fd33b6eee46a7eac9890e3a499b15faf
chenguanzhou <2050743162@qq.com>
2024-06-13 22:52:52 +0800

    feat: 新增了新增菜品及其口味接口、菜品分页查询接口

---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/entity_admin/SysDishFlavorEntity.java
@@ -0,0 +1,40 @@
+package com.ruoyi.web.entity_admin;
+
+import com.ruoyi.common.annotation.Excel;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class SysDishFlavorEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    private Long id;
+
+    /**
+     * $column.columnComment
+     */
+    @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
+    private Long userId;
+
+    /**
+     * 菜品
+     */
+    @Excel(name = "菜品")
+    private Long dishId;
+
+    /**
+     * 口味名称
+     */
+    @Excel(name = "口味名称")
+    private String name;
+
+    /**
+     * 口味数据list
+     */
+    @Excel(name = "口味数据list")
+    private String value;
+}
---
+++ ruoyi-admin/src/main/java/com/ruoyi/web/entity_admin/dto/DishPageQueryDTO.java
@@ -0,0 +1,28 @@
+package com.ruoyi.web.entity_admin.dto;
+
+import lombok.Data;
+import java.io.Serializable;
+
+/**
+ * 菜品分页
+ *
+ * @author ChenGuanzhou
+ * @email 2050743162@qq.com
+ * @date 2024-06-13
+ */
+@Data
+public class DishPageQueryDTO implements Serializable {
+
+    private int page;
+
+    private int pageSize;
+
+    //菜品名称
+    private String name;
+
+    //分类d
+    private Integer categoryId;
+
+    //状态0表示禁用1表示启用
+    private Integer status;
+}
